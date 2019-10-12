526ba9f29fe3e6ee9d869a18b4566d978f351f66
356 <youngjie999@gmail.com>
2019-10-12 11:16:35 +0800

    Update weather.lua
    更改了显示排列

--- weather/weather.lua
+++ weather/weather.lua
@@ -6,7 +6,7 @@ local menubar = hs.menubar.new()
 local menuData = {}
 
 local weaEmoji = {
-   lei = '⚡️雷',
+   lei = '⚡️',
    qing = '☀️晴',
    shachen = '😷沙尘暴',
    wu = '🌫雾',
@@ -36,12 +36,12 @@ function getWeather()
       for k, v in pairs(rawjson.data) do
          if k == 1 then
             menubar:setTitle(v.tem)
-            titlestr = string.format("%s %s %s 当前🌡️%s， 最低🌡%s， 最高🌡️%s， 💧%s 💨%s 🐸🌬 %s %s", city,weaEmoji[v.wea_img],v.day, v.tem, v.tem2, v.tem1, v.humidity, v.air, v.win_speed, v.wea)
+            titlestr = string.format("%s %s %s 当前🌡️%s， 最低🌡%s， 最高🌡️%s， 空气质量%s， %s", city,weaEmoji[v.wea_img],v.date, v.tem, v.tem2, v.tem1, v.air_level, v.wea)
             item = { title = titlestr }
             table.insert(menuData, item)
             table.insert(menuData, {title = '-'})
          else
-            titlestr = string.format("%s %s %s 最低🌡️%s， 最高🌡️%s， 🐸🌬%s %s", city, weaEmoji[v.wea_img],v.day, v.tem2, v.tem1, v.win_speed, v.wea)
+            titlestr = string.format("%s %s %s %s 最低🌡️%s， 最高🌡️%s， %s", city, weaEmoji[v.wea_img],v.date, v.week, v.tem2, v.tem1, v.wea)
             item = { title = titlestr }
             table.insert(menuData, item)
          end
